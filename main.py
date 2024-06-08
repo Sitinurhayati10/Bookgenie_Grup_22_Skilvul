12785cc3e96a9546530ebf0c1315a9732aee3220
EdwinprasW <weather2402@gmail.com>
2024-06-09 01:18:14 +0700

    changing the code for pkl download

--- main.py
+++ main.py
@@ -11,31 +11,33 @@ import requests
 import gzip 
 
 def download_from_gdrive(id, destination):
-    URL = "https://docs.google.com/uc?export=download"
-    session = requests.Session()
-    response = session.get(URL, params={'id': id}, stream=True)
-    token = get_confirm_token(response)
+        URL = "https://docs.google.com/uc?export=download"
 
-    if token:
-        params = {'id': id, 'confirm': token}
-        response = session.get(URL, params=params, stream=True)
+        session = requests.Session()
 
-    save_response_content(response, destination)
-    return destination
+        response = session.get(URL, params = { 'id' : id }, stream = True)
+        token = get_confirm_token(response)
+
+        if token:
+            params = { 'id' : id, 'confirm' : token }
+            response = session.get(URL, params = params, stream = True)
+
+            save_response_content(response, destination)    
+            return destination
 
 def get_confirm_token(response):
     for key, value in response.cookies.items():
         if key.startswith('download_warning'):
             return value
-        
+
     return None
 
 def save_response_content(response, destination):
     CHUNK_SIZE = 32768
-    
+
     with open(destination, "wb") as f:
         for chunk in response.iter_content(CHUNK_SIZE):
-            if chunk:  # filter out keep-alive new chunks
+            if chunk: # filter out keep-alive new chunks
                 f.write(chunk)
 
 def load_pickle(filename):
